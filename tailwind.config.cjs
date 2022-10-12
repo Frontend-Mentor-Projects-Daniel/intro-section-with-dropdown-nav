123725c3f50c4bda8b1b12b6ae5eabcbbddfccb1
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-10-12 02:32:45 -0400

    create base css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: 'hsl(0, 0%, 8%)',
+        'medium-grey': 'hsl(0, 0%, 41%)',
+        'almost-white': 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
