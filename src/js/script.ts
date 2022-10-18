e4c91bd19eabc58c0e1193b095116e3d7ccc97c0
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-10-18 18:42:27 -0400

    fix dropdown functionality

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,40 +1,103 @@
 export {};
 
-const menuItems = document.querySelectorAll('.tab.has-submenu');
-const buttons = document.querySelectorAll('button.with-icon');
+// GLOBAL VARIABLES
+const navbar = document.querySelector('.header__navbar') as HTMLElement;
+const menuButton = document.querySelector('.menu') as HTMLButtonElement;
+const menuImage = document.querySelector('.menu > img') as HTMLImageElement;
+const tabs = Array.from(
+  document.querySelectorAll('.tab.has-submenu')
+) as HTMLLIElement[];
+const selectButtons = Array.from(
+  document.querySelectorAll('button.with-icon')
+) as HTMLButtonElement[];
+const subMenus = Array.from(
+  document.querySelectorAll('.sub-menu')
+) as HTMLUListElement[];
 
-function closeTab(element: any) {
-  setTimeout(() => {
-    element.className = 'tab has-submenu';
-  }, 2000);
-}
+// toggle side nav on menu click
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('active');
 
-// accessibility for mouse users
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener('mouseover', function () {
-    menuItem.className = 'tab has-submenu open';
+  if (navbar.className == '[ header__navbar ] active') {
+    menuButton.setAttribute('aria-expanded', 'true');
+
+    menuImage.setAttribute('src', './src/assets/images/icon-close-menu.svg');
+  } else {
+    menuButton.setAttribute('aria-expanded', 'false');
+
+    menuImage.setAttribute('src', './src/assets/images/icon-menu.svg');
+  }
+});
+
+// open sub-menu on hover
+tabs.forEach(function (tab) {
+  tab.addEventListener('mouseover', function () {
+    tab.className = '[ tab has-submenu ] open';
+
+    if ((tab.className = '[ tab has-submenu ] open')) {
+      tab.firstElementChild?.nextElementSibling?.setAttribute(
+        'aria-expanded',
+        'true'
+      );
+
+      tab.firstElementChild?.setAttribute('aria-expanded', 'true');
+
+      tab.firstElementChild?.nextElementSibling?.firstElementChild?.firstElementChild?.setAttribute(
+        'href',
+        './src/assets/images/sprite.svg#icon-arrow-up'
+      );
+    }
   });
 
-  menuItem.addEventListener('mouseout', function () {
-    closeTab(menuItem);
+  tab.addEventListener('mouseout', function () {
+    closeTab(tab);
   });
 });
 
-// accessibility for keyboard users
-buttons.forEach((button) => {
+// open sub-menu on enter key or click
+selectButtons.forEach((button) => {
   button.addEventListener('click', function () {
-    if (button.parentElement?.className == 'tab has-submenu') {
-      button.parentElement.className = 'tab has-submenu open';
+    if (button.parentElement?.className == '[ tab has-submenu ]') {
+      button.parentElement.className = '[ tab has-submenu ] open';
 
       button.previousElementSibling?.setAttribute('aria-expanded', 'true');
       button.setAttribute('aria-expanded', 'true');
+      button?.firstElementChild?.firstElementChild?.setAttribute(
+        'href',
+        './src/assets/images/sprite.svg#icon-arrow-up'
+      );
     } else {
       if (button.parentElement) {
-        button.parentElement.className = 'tab has-submenu';
+        button.parentElement.className = '[ tab has-submenu ]';
       }
 
       button.previousElementSibling?.setAttribute('aria-expanded', 'false');
       button.setAttribute('aria-expanded', 'false');
+      button?.firstElementChild?.firstElementChild?.setAttribute(
+        'href',
+        './src/assets/images/sprite.svg#icon-arrow-down'
+      );
     }
   });
 });
+
+// FUNCTIONS
+// close sub-menu after 2 seconds
+function closeTab(tab: any) {
+  const timer = setTimeout(() => {
+    tab.className = '[ tab has-submenu ]';
+
+    tab.firstElementChild?.nextElementSibling?.setAttribute(
+      'aria-expanded',
+      'false'
+    );
+
+    tab.firstElementChild?.setAttribute('aria-expanded', 'false');
+    tab.firstElementChild?.nextElementSibling?.firstElementChild?.firstElementChild?.setAttribute(
+      'href',
+      './src/assets/images/sprite.svg#icon-arrow-down'
+    );
+  }, 2000);
+
+  return timer;
+}
